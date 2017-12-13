fef02816b55c7cc9e6dec3343ac362ce0e1b88a7
Federico Knüssel <fknussel@gmail.com>
2017-12-13 15:26:32 -0300

    Export Alert and Spinner component

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-import classnames from 'classnames';
 import closeIcon from './close.svg';
 import './Alert.scss';
 
@@ -12,7 +11,7 @@ type Props = {
 
 export default function Alert(props: Props) {
   return (
-    <div className={classnames('Alert', `Alert--${props.type}`)}>
+    <div className={`Alert Alert--${props.type}`}>
       {props.children}
 
       <a onClick={props.onClose} className="Alert__close-control">
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import { example } from './i18n/example';
 import Foo from './components/Foo/Foo';
+import Alert from './components/Alert/Alert';
+import Spinner from './components/Spinner/Spinner';
 import './index.scss';
 
 module.exports = {
+  example,
   Foo,
-  example
+  Alert,
+  Spinner
 };
